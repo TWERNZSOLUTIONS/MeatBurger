0fbc547eda39f7880df233fbfcef87e69664dada
TWERNZSOLUTIONS <thiago.wernz1@Ggmail.com>
2025-11-27 20:52:38 -0300

    Deploy: atualização da VITE_API_URL e ajustes gerais frontend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
   console.log(`🔥 Server running on port ${PORT}`);
